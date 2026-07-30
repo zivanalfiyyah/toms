fedeb124f96c7f3e5c0aa6588e5a0e76df9153b5
zivanalfiyyah <alfiyyahzivana@gmail.com>
2026-07-30 19:29:39 +0700

    Chore: add TomsStructure and update DatabaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             RolePermissionSeeder::class,
+            TomsStructureSeeder::class,
         ]);
 
         User::factory()->create([
