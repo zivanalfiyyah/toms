7976dec51eb3dbdc7fc2ada6560e8ce4adf92a86
zivanalfiyyah <alfiyyahzivana@gmail.com>
2026-08-10 10:26:12 +0700

    save router change

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import CategoryPage from '../views/CategoryPage.vue'
-import PageIndexPage from '../views/PageIndexPage.vue'
 import DocPage from '../views/DocPage.vue'
 import LoginPage from '../views/LoginPage.vue'
 import EditPage from '../views/EditPage.vue'
@@ -10,10 +9,26 @@ const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/login', name: 'login', component: LoginPage },
   { path: '/docs/:category', name: 'category-page', component: CategoryPage, props: true },
-  { path: '/docs/:category/:page', name: 'page-index', component: PageIndexPage, props: true },
-  { path: '/docs/:category/:page/edit', name: 'edit-page-only', component: EditPage, props: true },
-  { path: '/docs/:category/:page/:child', name: 'doc-page', component: DocPage, props: true },
-  { path: '/docs/:category/:page/:child/edit', name: 'edit-page', component: EditPage, props: true }
+
+  {
+    path: '/docs/:category/:slugs+/edit',
+    name: 'edit-page',
+    component: EditPage,
+    props: (route) => ({
+      category: route.params.category,
+      slugs: Array.isArray(route.params.slugs) ? route.params.slugs : [route.params.slugs]
+    })
+  },
+
+  {
+    path: '/docs/:category/:slugs+',
+    name: 'doc-page',
+    component: DocPage,
+    props: (route) => ({
+      category: route.params.category,
+      slugs: Array.isArray(route.params.slugs) ? route.params.slugs : [route.params.slugs]
+    })
+  }
 ]
 
 const router = createRouter({
