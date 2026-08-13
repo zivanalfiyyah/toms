6fc60bc0a039a3fced2ea230bc9b4b9883558c0d
zivanalfiyyah <alfiyyahzivana@gmail.com>
2026-08-13 09:38:52 +0700

    Update cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -23,6 +23,7 @@ return [
         'http://localhost:5173',
         'http://localhost:3000',
         'http://127.0.0.1:5173',
+        'https://comfortable-spirit-production.up.railway.app',
     ],
 
     'allowed_origins_patterns' => [],
